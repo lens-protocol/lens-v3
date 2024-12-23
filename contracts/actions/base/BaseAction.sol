3f2e673642def5f150cc9ee9a0678f1089dc4426
vicnaum <vicnaum@gmail.com>
2024-12-23 20:25:37 +0100

    misc: baseAction moved slot out

--- contracts/actions/base/BaseAction.sol
+++ contracts/actions/base/BaseAction.sol
@@ -7,22 +7,19 @@ import {UNIVERSAL_ACTION_MAGIC_VALUE} from "./../../dashboard/actions/ActionHub.
 abstract contract BaseAction {
     address immutable ACTION_HUB;
 
+    // keccak256("lens.core.storage.action.universal.configured");
+    bytes32 constant UNIVERSAL_CONFIGURED_SLOT = 0x0c15cbaf7a02c80aa477dd966ee255b2a1af32b54b8ade0c0a6bbed64aa9142c;
+
     modifier onlyActionHub() {
         require(msg.sender == ACTION_HUB);
         _;
     }
 
-    constructor(
-        address actionHub
-    ) {
+    constructor(address actionHub) {
         ACTION_HUB = actionHub;
     }
 
-    function _configureUniversalAction(
-        address originalMsgSender
-    ) internal onlyActionHub returns (bytes memory) {
-        // uint256(keccak256("lens.core.storage.action.universal.configured"));
-        uint256 UNIVERSAL_CONFIGURED_SLOT = uint256(0x0c15cbaf7a02c80aa477dd966ee255b2a1af32b54b8ade0c0a6bbed64aa9142c);
+    function _configureUniversalAction(address originalMsgSender) internal onlyActionHub returns (bytes memory) {
         bool configured;
         assembly {
             configured := sload(UNIVERSAL_CONFIGURED_SLOT)
