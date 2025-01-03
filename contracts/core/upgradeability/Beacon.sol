2ac70f4595132ec7790172a192556aab2a20592c
Alan Donoso Naumczuk <adonoso@itba.edu.ar>
2025-01-03 20:52:41 -0300

    feat: Beacon events and ownable added

--- contracts/core/upgradeability/Beacon.sol
+++ contracts/core/upgradeability/Beacon.sol
@@ -2,12 +2,24 @@
 // Copyright (C) 2024 Lens Labs. All Rights Reserved.
 pragma solidity ^0.8.0;
 
+import {Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
 import {IVersionedBeacon} from "contracts/core/interfaces/IVersionedBeacon.sol";
 
-contract Beacon is IVersionedBeacon {
-    address internal _owner; // TODO: Ownable
+contract Beacon is Ownable2Step, IVersionedBeacon {
+    event ImplementationSetForVersion(uint256 indexed version, address indexed implementation);
+    event DefaultVersionSet(uint256 indexed version);
+
     mapping(uint256 => address) internal _implementations;
     uint256 internal _defaultVersion;
+
+    constructor(address owner, uint256 version, address initialImplementation) Ownable2Step() {
+        _transferOwnership(owner);
+        require(initialImplementation != address(0));
+        _implementations[version] = initialImplementation;
+        emit ImplementationSetForVersion(version, initialImplementation);
+        _defaultVersion = version;
+        emit DefaultVersionSet(version);
+    }
 
     function implementation() external view override returns (address) {
         return _implementations[_defaultVersion];
@@ -19,16 +31,17 @@ contract Beacon is IVersionedBeacon {
         return implementationByVersion;
     }
 
-    function setImplementationForVersion(uint256 version, address implementationToSet) external {
-        require(msg.sender == _owner);
+    function setImplementationForVersion(uint256 version, address implementationToSet) external onlyOwner {
+        if (_defaultVersion == version) {
+            require(implementationToSet != address(0));
+        }
         _implementations[version] = implementationToSet;
-        // event
+        emit ImplementationSetForVersion(version, implementationToSet);
     }
 
-    function setDefaultVersion(uint256 version) external {
-        require(msg.sender == _owner);
+    function setDefaultVersion(uint256 version) external onlyOwner {
         require(_implementations[version] != address(0));
         _defaultVersion = version;
-        // event
+        emit DefaultVersionSet(version);
     }
 }
