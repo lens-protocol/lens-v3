6b3bc96965c417d731755fb05c31179b384d08ff
Alan Donoso Naumczuk <adonoso@itba.edu.ar>
2025-01-09 01:17:29 -0300

    test: MockAccessControlLib added as syntax sugar

---
+++ test/helpers/MockAccessControlLib.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+// Copyright (C) 2024 Lens Labs. All Rights Reserved.
+pragma solidity 0.8.17;
+
+import {AccessControlled} from "contracts/core/access/AccessControlled.sol";
+import {MockAccessControl} from "test/mocks/MockAccessControl.sol";
+
+library MockAccessControlLib {
+    function mockAccess(
+        address accessControlledContract,
+        address account,
+        address contractAddress,
+        uint256 permissionId,
+        bool access
+    ) internal {
+        _getMockAccessControl(accessControlledContract).mockAccess(account, contractAddress, permissionId, access);
+    }
+
+    function mockCanChangeAccessControl(
+        address accessControlledContract,
+        address account,
+        address contractAddress,
+        bool canChange
+    ) internal {
+        _getMockAccessControl(accessControlledContract).mockCanChangeAccessControl(account, contractAddress, canChange);
+    }
+
+    function _getMockAccessControl(address accessControlledContract) private view returns (MockAccessControl) {
+        return MockAccessControl(address(AccessControlled(accessControlledContract).getAccessControl()));
+    }
+}
