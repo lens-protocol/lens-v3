0ba5dd181c2ef0e595e7e7f89cbaba4fe8c146bb
vicnaum <vicnaum@gmail.com>
2025-01-10 11:36:02 +0100

    misc: RuleBasePrimitive made to compile without IR

--- contracts/core/primitives/graph/RuleBasedGraph.sol
+++ contracts/core/primitives/graph/RuleBasedGraph.sol
@@ -67,7 +67,7 @@ abstract contract RuleBasedGraph is IGraph, RuleBasedPrimitive {
         return selectors;
     }
 
-    function _encodePrimitiveConfigureCall(bytes32 configSalt, KeyValue[] calldata ruleParams)
+    function _encodePrimitiveConfigureCall(bytes32 configSalt, KeyValue[] memory ruleParams)
         internal
         pure
         override
@@ -80,7 +80,7 @@ abstract contract RuleBasedGraph is IGraph, RuleBasedPrimitive {
         bool wasAlreadyConfigured,
         address ruleAddress,
         bytes32 configSalt,
-        KeyValue[] calldata ruleParams
+        KeyValue[] memory ruleParams
     ) internal override {
         if (wasAlreadyConfigured) {
             emit IGraph.Lens_Graph_RuleReconfigured(ruleAddress, configSalt, ruleParams);
--- contracts/core/primitives/namespace/RuleBasedNamespace.sol
+++ contracts/core/primitives/namespace/RuleBasedNamespace.sol
@@ -46,7 +46,7 @@ abstract contract RuleBasedNamespace is INamespace, RuleBasedPrimitive {
         return selectors;
     }
 
-    function _encodePrimitiveConfigureCall(bytes32 configSalt, KeyValue[] calldata ruleParams)
+    function _encodePrimitiveConfigureCall(bytes32 configSalt, KeyValue[] memory ruleParams)
         internal
         pure
         override
@@ -59,7 +59,7 @@ abstract contract RuleBasedNamespace is INamespace, RuleBasedPrimitive {
         bool wasAlreadyConfigured,
         address ruleAddress,
         bytes32 configSalt,
-        KeyValue[] calldata ruleParams
+        KeyValue[] memory ruleParams
     ) internal override {
         if (wasAlreadyConfigured) {
             emit INamespace.Lens_Namespace_RuleReconfigured(ruleAddress, configSalt, ruleParams);
