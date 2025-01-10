1098fdacb496f419bf0b0a493be022ed4edc4754
vicnaum <vicnaum@gmail.com>
2025-01-10 11:58:43 +0100

    misc: MigrationFeed made to compile without IR

--- contracts/migration/MigrationFeed.sol
+++ contracts/migration/MigrationFeed.sol
@@ -7,60 +7,76 @@ import {CreatePostParams} from "contracts/core/interfaces/IFeed.sol";
 import {FeedCore as Core, PostStorage} from "contracts/core/primitives/feed/FeedCore.sol";
 import {Feed} from "contracts/core/primitives/feed/Feed.sol";
 
+struct PostCreationParams {
+    uint256 postId;
+    uint256 rootPostId;
+    uint256 postSequentialId;
+    uint256 authorPostSequentialId;
+    uint80 creationTimestamp;
+    address source;
+}
+
 contract MigrationFeed is Feed {
     function createPost(
-        CreatePostParams calldata postParams,
-        KeyValue[] calldata customParams,
-        RuleProcessingParams[] calldata feedRulesParams,
-        RuleProcessingParams[] calldata rootPostRulesParams,
-        RuleProcessingParams[] calldata quotedPostRulesParams
+        CreatePostParams memory postParams,
+        KeyValue[] memory customParams,
+        RuleProcessingParams[] memory feedRulesParams,
+        RuleProcessingParams[] memory rootPostRulesParams,
+        RuleProcessingParams[] memory quotedPostRulesParams
     ) external override returns (uint256) {
-        (
-            uint256 postId,
-            uint256 rootPostId,
-            uint256 postSequentialId,
-            uint256 authorPostSequentialId,
-            uint80 creationTimestamp,
-            address source
-        ) = abi.decode(customParams[0].value, (uint256, uint256, uint256, uint256, uint80, address));
-        // _createPost(postParams, postId, rootPostId, postSequentialId, authorPostSequentialId, creationTimestamp);
+        PostCreationParams memory postCreationParams = abi.decode(customParams[0].value, (PostCreationParams));
+        _createPost(
+            postParams,
+            postCreationParams.postId,
+            postCreationParams.rootPostId,
+            postCreationParams.postSequentialId,
+            postCreationParams.authorPostSequentialId,
+            postCreationParams.creationTimestamp
+        );
 
         if (customParams.length > 1 && abi.decode(customParams[1].value, (bool))) {
             // If customParams[1] is present, it must be an ABI-encoded bool representing `forceChecks`
-            // _forceChecks(postId, rootPostId, postParams);
+            _forceChecks(postCreationParams.postId, postCreationParams.rootPostId, postParams);
         }
 
-        if (source != address(0)) {
+        if (postCreationParams.source != address(0)) {
             // Trust the migrator, no source verification
-            _setPrimitiveInternalExtraDataForEntity(postId, KeyValue(DATA__SOURCE, abi.encode(source)));
-            _setPrimitiveInternalExtraDataForEntity(postId, KeyValue(DATA__LAST_UPDATED_SOURCE, abi.encode(source)));
+            _setPrimitiveInternalExtraDataForEntity(
+                postCreationParams.postId, KeyValue(DATA__SOURCE, abi.encode(postCreationParams.source))
+            );
+            _setPrimitiveInternalExtraDataForEntity(
+                postCreationParams.postId, KeyValue(DATA__LAST_UPDATED_SOURCE, abi.encode(postCreationParams.source))
+            );
         }
 
-        // emit Lens_Feed_PostCreated(
-        //     source,
-        //     postId,
-        //     authorPostSequentialId,
-        //     rootPostId,
-        //     quotedPostRulesParams,
-        //     rootPostRulesParams,
-        //     feedRulesParams,
-        //     customParams,
-        //     postParams,
-        //     postParams.author
-        // );
+        emit Lens_Feed_PostCreated(
+            postCreationParams.postId,
+            postParams.author,
+            postCreationParams.authorPostSequentialId,
+            postCreationParams.rootPostId,
+            postParams,
+            customParams,
+            feedRulesParams,
+            rootPostRulesParams,
+            quotedPostRulesParams,
+            postCreationParams.source
+        );
 
-        // for (uint256 i = 0; i < postParams.extraData.length; i++) {
-        //     _setEntityExtraData(postId, postParams.extraData[i]);
-        //     emit Lens_Feed_Post_ExtraDataAdded(
-        //         postId, postParams.extraData[i].key, postParams.extraData[i].value, postParams.extraData[i].value
-        //     );
-        // }
-        return postId;
+        for (uint256 i = 0; i < postParams.extraData.length; i++) {
+            _setEntityExtraData(postCreationParams.postId, postParams.extraData[i]);
+            emit Lens_Feed_Post_ExtraDataAdded(
+                postCreationParams.postId,
+                postParams.extraData[i].key,
+                postParams.extraData[i].value,
+                postParams.extraData[i].value
+            );
+        }
+        return postCreationParams.postId;
     }
 
     // Overriding the FeedCore
     function _createPost(
-        CreatePostParams calldata postParams,
+        CreatePostParams memory postParams,
         uint256 postId,
         uint256 rootPostId,
         uint256 postSequentialId,
@@ -82,7 +98,7 @@ contract MigrationFeed is Feed {
         _newPost.lastUpdatedTimestamp = creationTimestamp;
     }
 
-    function _forceChecks(uint256 postId, uint256 rootPostId, CreatePostParams calldata postParams) internal view {
+    function _forceChecks(uint256 postId, uint256 rootPostId, CreatePostParams memory postParams) internal view {
         // TODO: Check if the rootPostId == postId case (not a reply, not a repost)
         if (rootPostId != postId) {
             require(Core._postExists(rootPostId));
