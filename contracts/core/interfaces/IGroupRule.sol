959fe036b8a340ea32a4d99caccda2f15d1287c8
Alan Donoso Naumczuk <adonoso@itba.edu.ar>
2025-02-07 17:33:46 -0300

    misc: Warning comments added to IGroupRule

--- contracts/core/interfaces/IGroupRule.sol
+++ contracts/core/interfaces/IGroupRule.sol
@@ -7,6 +7,14 @@ import {KeyValue} from "contracts/core/types/Types.sol";
 interface IGroupRule {
     function configure(bytes32 configSalt, KeyValue[] calldata ruleParams) external;
 
+    /**
+     * The `processAddition` logic should be written so that it can serve as a SUFFICIENT condition when it is
+     * the only rule applied. In other words, it should be capable of safely handling the entire membership
+     * addition flow on its own.
+     *
+     * If, however, `processAddition` only addresses part of the required member addition logic (i.e. it is NECESSARY
+     * BUT NOT SUFFICIENT), you may encounter unexpected behavior when it is not combined with any complementary rules.
+     */
     function processAddition(
         bytes32 configSalt,
         address originalMsgSender,
@@ -15,6 +23,14 @@ interface IGroupRule {
         KeyValue[] calldata ruleParams
     ) external;
 
+    /**
+     * The `processRemoval` logic should be written so that it can serve as a SUFFICIENT condition when it is
+     * the only rule applied. In other words, it should be capable of safely handling the entire membership
+     * removal flow on its own.
+     *
+     * If, however, `processRemoval` only addresses part of the required member removal logic (i.e. it is NECESSARY
+     * BUT NOT SUFFICIENT), you may encounter unexpected behavior when it is not combined with any complementary rules.
+     */
     function processRemoval(
         bytes32 configSalt,
         address originalMsgSender,
