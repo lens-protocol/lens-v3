54aef5a621a195e02e7da05646b3e6789197f35b
Alan Donoso Naumczuk <adonoso@itba.edu.ar>
2025-03-04 01:28:21 -0300

    test: Initalizable tested - Upgradeability folder coverage raised to 100%

---
+++ test/upgradeability/Initializable.t.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: UNLICENSED
+// Copyright (C) 2024 Lens Labs. All Rights Reserved.
+pragma solidity ^0.8.26;
+
+import "forge-std/Test.sol";
+import {Initializable} from "@core/upgradeability/Initializable.sol";
+import {Errors} from "@core/types/Errors.sol";
+
+contract InitializableContract is Initializable {
+    function initialize() public initializer {
+        return;
+    }
+}
+
+contract InitializableTest is Test {
+    /// @custom:keccak lens.storage.Initializable
+    bytes32 constant STORAGE__INITIALIZABLE = 0xbd2c04feebbff2d29fe1b04edf9a1d94ba7a836bad797bdd99c9e722e172cdd0;
+
+    bytes32 constant TRUE = bytes32(uint256(1));
+    bytes32 constant FALSE = bytes32(uint256(0));
+
+    InitializableContract initializable;
+
+    function setUp() public virtual {
+        initializable = new InitializableContract();
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    function test_Initialize() public {
+        assertEq(vm.load(address(initializable), STORAGE__INITIALIZABLE), FALSE);
+
+        initializable.initialize();
+
+        assertEq(vm.load(address(initializable), STORAGE__INITIALIZABLE), TRUE);
+    }
+
+    function test_Cannot_InitializeTwice() public {
+        assertEq(vm.load(address(initializable), STORAGE__INITIALIZABLE), FALSE);
+
+        initializable.initialize();
+
+        assertEq(vm.load(address(initializable), STORAGE__INITIALIZABLE), TRUE);
+
+        vm.expectRevert(Errors.AlreadyInitialized.selector);
+        initializable.initialize();
+    }
+}
