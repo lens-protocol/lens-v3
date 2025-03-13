775344b86d945af02df96bd2a739f5f2b2446fcc
vicnaum <vicnaum@gmail.com>
2025-03-13 19:41:45 +0100

    deploy: New Testnet Deployed

--- deploy/upgradeSingleContract.ts
+++ deploy/upgradeSingleContract.ts
@@ -20,10 +20,21 @@ async function deploy() {
   const contractToUpgrade: ContractInfo =
     // Factories
     {
-      name: 'AccessControlFactoryImpl',
-      contractName: 'AccessControlFactory',
+      name: 'LensFactoryImpl',
+      address: '0x37a998C7A641A9440B96b2eA43Ef8c0ED7d19435',
+      contractName: 'LensFactory',
       contractType: ContractType.Factory,
-      constructorArguments: [loadContractAddressFromAddressBook('AccessControlLock')],
+      constructorArguments: [        "0xF25BEeF04aCC3550802B00e0bE45Dd593b73E4B7",
+        "0x7447a4C251399E007719Eb61A83A21304443E5f1",
+        "0x82c4443fCe54067a88f1FCf1753621489b3A1DE1",
+        "0xC1D3e9c18Dae5184d8DF8E007F8Ea2849ECDe792",
+        "0x8A3FE85B06142ED92ee889756D578C95c96bcdBC",
+        "0x1355592A42eDA9C69C661d7762a6e53a27276E23",
+        "0x03afEFa385ACee20349da88Fb107CDb9d5BE881e",
+        "0x87808bdabbBB66Cc043a83F9327771a5218ca1f5",
+        "0x002A6418581A2C14C7b239cf1cE7AF0F2Baf2B85",
+        "0xCEd3d4F71934F9712449a14c06135283Dcdad260",
+        "0x48a6983d0C7FF6bF24a5b1dBbE0C09Ea37b654b7"],
     };
 
   if (contractToUpgrade.constructorArguments === undefined) {
@@ -47,14 +58,22 @@ async function deploy() {
   const oldImplementation = await hre.upgrades.erc1967.getImplementationAddress(transparentUpgradeableProxyAddress);
   console.log(`Old implementation in the Proxy: ${oldImplementation}`);
 
-  const deployedImplementation = await deployContract(
-    contractToUpgrade.contractName,
-    contractToUpgrade.constructorArguments
-  );
+  let deployedImplementationAddress;
 
-  console.log(
-      `${contractToUpgrade.contractName} implementation deployed at ${await deployedImplementation.getAddress()}`
-  );
+  if (contractToUpgrade.address) {
+    deployedImplementationAddress = contractToUpgrade.address;
+    console.log(
+      `${contractToUpgrade.contractName} implementation already deployed at ${deployedImplementationAddress}`
+    );
+  } else {
+    deployedImplementationAddress = (await deployContract(
+      contractToUpgrade.contractName,
+      contractToUpgrade.constructorArguments
+    )).getAddress();
+    console.log(
+      `${contractToUpgrade.contractName} implementation deployed at ${deployedImplementationAddress}`
+    );
+  }
 
   const proxyOwnerWallet = getWallet(proxyOwnerPrivateKey);
 
@@ -62,13 +81,13 @@ async function deploy() {
   const transparentUpgradeableProxy = new ethers.Contract(transparentUpgradeableProxyAddress, transparentUpgradeableProxyArtifact.abi, proxyOwnerWallet);
 
   const upgradeTx = await transparentUpgradeableProxy.upgradeTo(
-    await deployedImplementation.getAddress()
+    deployedImplementationAddress
   );
   await upgradeTx.wait();
 
   const newImplementation = await hre.upgrades.erc1967.getImplementationAddress(transparentUpgradeableProxyAddress);
 
-  if (newImplementation !== await deployedImplementation.getAddress()) {
+  if (newImplementation !== deployedImplementationAddress) {
     throw new Error(`${contractToUpgrade.contractName} upgrade failed`);
   }
 
