1e5b255180f306d04bc28dc6f929adddc4864edd
Alan Donoso Naumczuk <adonoso@itba.edu.ar>
2025-03-21 17:08:35 -0300

    misc: Refactoring the isZkEvm to actually work : )
    Co-authored-by: Victor Naumik <vicnaum@gmail.com>

--- test/helpers/ZkTest.sol
+++ test/helpers/ZkTest.sol
@@ -4,9 +4,16 @@ pragma solidity ^0.8.26;
 
 import "forge-std/Test.sol";
 
-contract ZkTest is Test {
+contract ZkTester {
     function isZkEvm() public view returns (bool) {
         return block.coinbase == address(0x8001);
+    }
+}
+
+contract ZkTest is Test {
+    function isZkEvm() public returns (bool) {
+        ZkTester zkTester = new ZkTester();
+        return zkTester.isZkEvm();
     }
 
     modifier onlyZkEvm() {
