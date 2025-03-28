1126b2c6f2f299812b5d985bb4fb69fa1cc42080
vicnaum <vicnaum@gmail.com>
2025-03-28 19:13:04 +0100

    misc: LensCreate2 made Ownable

--- contracts/core/upgradeability/LensCreate2.sol
+++ contracts/core/upgradeability/LensCreate2.sol
@@ -6,7 +6,11 @@ import {
     TransparentUpgradeableProxy,
     ITransparentUpgradeableProxy
 } from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import {Ownable} from "contracts/core/access/Ownable.sol";
 import {Errors} from "contracts/core/types/Errors.sol";
+
+// If using the [0] nonce of 0xfe4Ad59637Cab6A5AbAEe896D3d01dA67f418e76 deployer:
+address constant LENS_CREATE_2 = 0x52AF9CF29976C310E3DE03C509E108edB6edb8c0;
 
 contract FixedImplementationContract {}
 
@@ -22,14 +26,14 @@ interface ILensCreate2 {
     ) external returns (address);
 }
 
-contract LensCreate2 is ILensCreate2 {
+contract LensCreate2 is ILensCreate2, Ownable {
     address public immutable FIXED_IMPLEMENTATION;
     bytes32 public immutable PROXY_BYTECODE_HASH;
     bytes32 public immutable SENDER_BYTES;
     bytes32 public immutable CREATE2_PREFIX;
     bytes32 public immutable CONSTRUCTOR_ARGS_HASH;
 
-    constructor() {
+    constructor(address owner) Ownable() {
         FIXED_IMPLEMENTATION = address(new FixedImplementationContract());
         address proxy = address(new TransparentUpgradeableProxy(FIXED_IMPLEMENTATION, address(this), ""));
         bytes32 bytecodeHash;
@@ -40,6 +44,7 @@ contract LensCreate2 is ILensCreate2 {
         CREATE2_PREFIX = keccak256("zksyncCreate2");
         SENDER_BYTES = bytes32(uint256(uint160(address(this))));
         CONSTRUCTOR_ARGS_HASH = keccak256(abi.encode(FIXED_IMPLEMENTATION, address(this), ""));
+        _transferOwnership(owner);
     }
 
     function getAddress(bytes32 salt) external view override returns (address) {
