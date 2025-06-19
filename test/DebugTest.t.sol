3ede26f76e1f94a3c8a2b81d5b7dc3b91918e279
Alan Donoso Naumczuk <adonoso@itba.edu.ar>
2025-06-19 17:44:19 -0300

    test: Added function to exclude some mock and tests files from coverage

--- test/DebugTest.t.sol
+++ test/DebugTest.t.sol
@@ -25,6 +25,10 @@ struct PaymentConfiguration {
 // This contraption can be used as a template for debugging on-chain transactions.
 
 contract DebugTest is Test, BaseDeployments {
+    function testDebugTest() public {
+        // Prevents being included in the foundry coverage report
+    }
+
     function setUp() public override onlyFork {
         // super.setUp();
         // address usernamePricePerLengthNamespaceRuleImpl = address(new UsernamePricePerLengthNamespaceRule());
--- test/mocks/MockLensCreate2.sol
+++ test/mocks/MockLensCreate2.sol
@@ -3,6 +3,10 @@
 pragma solidity ^0.8.26;
 
 contract MockLensCreate2 {
+    function testMockLensCreate2() public {
+        // Prevents being included in the foundry coverage report
+    }
+
     mapping(bytes32 => address) public addresses;
 
     function getAddress(bytes32 salt) public view returns (address) {
