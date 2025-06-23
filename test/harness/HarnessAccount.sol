f582ef216b2e2e1294700dc8d42b7df84f27a1e9
Alan Donoso Naumczuk <adonoso@itba.edu.ar>
2025-06-23 19:34:36 -0300

    misc: Warnings cleared & unrechable code supressed

--- test/harness/HarnessAccount.sol
+++ test/harness/HarnessAccount.sol
@@ -1,8 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+// Copyright (C) 2024 Lens Labs. All Rights Reserved.
 pragma solidity ^0.8.26;
 
 import {Account} from "contracts/extensions/account/Account.sol";
 import {IAccount} from "contracts/extensions/account/IAccount.sol";
-import {IOwnable} from "contracts/core/interfaces/IOwnable.sol";
 import {KeyValue} from "contracts/core/types/Types.sol";
 
 interface IHarnessAccount is IAccount {
