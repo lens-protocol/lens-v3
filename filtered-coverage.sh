f1774aefa4bdd18726c9266f62b02470e7f56af8
Alan Donoso Naumczuk <adonoso@itba.edu.ar>
2025-06-25 21:42:28 -0300

    misc: Filtered coverage output improved to be more friendly

--- filtered-coverage.sh
+++ filtered-coverage.sh
@@ -12,3 +12,10 @@ lcov --remove lcov.info "test/*" "contracts/actions/*" "contracts/migration/*" "
 genhtml --ignore-errors inconsistent --ignore-errors corrupt --ignore-errors category --rc derive_function_end_line=0 lcov_filtered.info -o coverage/html --branch-coverage >/dev/null 2>&1 || { echo "Error generating coverage report"; exit 1; }
 
 rm lcov_filtered.info
+
+echo -e "\n\n"
+echo -e "- - - - -\n"
+echo -e "Coverage report generated at:\n"
+echo -e "\t $PWD/coverage/html/index.html \n"
+echo -e "You can go to your browser and paste that path in the address bar to look at the report.\n"
+echo -e "- - - - -\n\n"
