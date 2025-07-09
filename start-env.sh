618f16412fcd9516e94d8cb9980c05a55de41d01
vicnaum <vicnaum@gmail.com>
2025-07-09 20:13:49 +0200

    misc: addressBook scripts small capitalization fix

--- start-env.sh
+++ start-env.sh
@@ -7,15 +7,15 @@ if [ "$1" == "mainnet" ]; then
   echo "mainnet" > .env.active
   cp .env.mainnet .env
   echo "○ .env.mainnet copied to .env"
-  cp addressbook.mainnet.json addressbook.json
-  echo "○ addressbook.mainnet.json copied to addressbook.json"
+  cp addressBook.mainnet.json addressBook.json
+  echo "○ addressBook.mainnet.json copied to addressBook.json"
   echo "⦿ Environment ready for mainnet!"
 elif [ "$1" == "testnet" ]; then
   echo "testnet" > .env.active
   cp .env.testnet .env
   echo "○ .env.testnet copied to .env"
-  cp addressbook.testnet.json addressbook.json
-  echo "○ addressbook.testnet.json copied to addressbook.json"
+  cp addressBook.testnet.json addressBook.json
+  echo "○ addressBook.testnet.json copied to addressBook.json"
   echo "⦿ Environment ready for testnet!"
 else
   echo "Invalid argument: $1"
