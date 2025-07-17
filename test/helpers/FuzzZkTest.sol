02a91468c2a65b3e3e50c78327051e20ddd64c2a
Alan Donoso Naumczuk <adonoso@itba.edu.ar>
2025-07-17 14:07:05 -0300

    test: Bound for block timestamp fixed

--- test/helpers/FuzzZkTest.sol
+++ test/helpers/FuzzZkTest.sol
@@ -7,17 +7,17 @@ import "./ZkTest.sol";
 
 contract FuzzZkTest is ZkTest {
     function _boundAmount(uint256 amount) internal pure returns (uint256) {
-        // Bound amount (0, 2^95), as test contract's native balance is 2^96, and vm.deal has issues in zksync foundry
+        // Bound amount (0, 2^95], as test contract's native balance is 2^96, and vm.deal has issues in zksync foundry
         return bound(amount, 1, 1 << 95);
     }
 
     function _boundAmountAllowZero(uint256 amount) internal pure returns (uint256) {
-        // Bound amount [0, 2^95), as test contract's native balance is 2^96, and vm.deal has issues in zksync foundry
+        // Bound amount [0, 2^95], as test contract's native balance is 2^96, and vm.deal has issues in zksync foundry
         return bound(amount, 0, 1 << 95);
     }
 
     function _boundBlockTimestamp(uint256 blockTimestamp) internal pure returns (uint256) {
         // Bound block timestamp (0, 2^64), as vm.warp expects a timestamp smaller than 2^64
-        return bound(blockTimestamp, 1, 1 << 64);
+        return bound(blockTimestamp, 1, 1 << 64 - 1);
     }
 }
